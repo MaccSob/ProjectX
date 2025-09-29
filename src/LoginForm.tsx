c21fe607ec40481eb501c18904100aea66b07305
MaccSob <71sobmaciej@gmail.com>
2025-09-29 23:53:11 +0200

    fixed forms

--- src/LoginForm.tsx
+++ src/LoginForm.tsx
@@ -5,17 +5,24 @@ export const LoginForm = () => {
         <>
         <form action="">
 
-        <h3>Register</h3>
+        <h3>Login</h3>
+        <div className="form_group">
         <label htmlFor="e-mail">E-mail</label>
         <input type="email" placeholder="Enter your e-mail" name="email" id="email" />
+            </div>
             
+            <div className="form_group">
             <label htmlFor="login">Login</label>
         <input type="text" placeholder="Enter your Login" name="login" id="login" />
+            </div>
             
-        <label htmlFor="login">Login</label>
+            <div className="form_group">
+
+           
+        <label htmlFor="login">Password</label>
 
         <input type="password" name="password" id="password" />
-
+ </div>
         </form>
         </>
     )
--- src/RegisterForm.tsx
+++ src/RegisterForm.tsx
@@ -4,12 +4,40 @@ export const FormRegister = () => {
     return (
         <>
         <form action="">
+      
+        <h3>Create your account!</h3>
 
-        
+        <div className="form_group">
 
+        <label htmlFor="e-mail">E-mail</label>
+        <input type="email" placeholder="Enter your e-mail" name="email" id="email" />
             
+            </div>
+            
+            <div className="form_group">
+
+            <label htmlFor="login">Login</label>
+        <input type="text" placeholder="Enter your Login" name="login" id="login" />
+      
+           </div>
+
+            <div className="form_group">
+
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+          
+            </div>
+
+           
+            <div className="form_group">
+      <label htmlFor="cpassword">Confirm password</label>
+        <input type="password" name="cpassword" id="cpassword" />
+    
+            </div>
+  
         </form>
         </>
+
     )
 
 }
